11b0b683f813da346b18377d0d5325fe933d3c6a
Dinesh Kumar <114287171+kumardinesh21@users.noreply.github.com>
2024-10-08 13:36:20 +0500

    Intial-setup

---
+++ Service-Registery/src/test/java/com/registery/Service_Registery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registery.Service_Registery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
