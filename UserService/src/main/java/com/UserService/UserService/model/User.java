f0ecdf97b7d374796375028ea5b20492685ed578
Dinesh Kumar <114287171+kumardinesh21@users.noreply.github.com>
2024-10-09 22:14:18 +0500

    Adding User Service

---
+++ UserService/src/main/java/com/UserService/UserService/model/User.java
@@ -0,0 +1,32 @@
+package com.UserService.UserService.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Getters and Setters
+}
+
+
